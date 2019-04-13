5e1f9e9f3f690954a4ac6075e987283e012d0d1c
Elise Phan <aliceconnic@gmail.com>
2019-04-13 21:54:18 +0000

    Automatically backed up by Learn

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 export default class DelayedButton extends React.Component {
 
   handleMouseMovement = (event) => {
-    this.props.onReceiveCoordinates([event.clientX, event.clientY]);
+    this.props.onDelayedClick([event.clientX, event.clientY]);
     })
   }
   
