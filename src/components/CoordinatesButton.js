7a442fc21696966878f87ecf8c71c625be9e6c29
Elise Phan <aliceconnic@gmail.com>
2019-04-13 21:45:29 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -3,5 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export default class CoordinatesButton extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      mouseCoordinates: this.props.mouseCoordinates
+    }
+  }
   
+  handleMouseMovement = () => {
+    const newMouseMovement = this.props.mouseCoordinates
+  }
 }
